60af56c48d6613ce732a69130f9435e8f6b3a8bd
huongnq <huongnq4@cyberspace.vn>
2019-11-27 09:24:21 +0700

    add uitls infixToPostfix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import utils.NotationUtils;
+
+
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println("BT01-1612247!");
+        System.out.println(NotationUtils.infixToPostfix("2*((3+4)/5)"));
+
     }
 }
